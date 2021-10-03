0162e5357dac7865ee35d79ad183a5448133df1c
maronin <final3n@hanmail.net>
2021-10-03 10:45:51 +0900

    [feat] env value

--- webview/webview.go
+++ webview/webview.go
@@ -1,13 +1,22 @@
 package webview
 
-import "github.com/webview/webview"
+import (
+	"fiber/utils"
+
+	"github.com/webview/webview"
+)
+
+var (
+	width  = utils.Env.Width
+	height = utils.Env.Height
+)
 
 func Webview() {
 	debug := true
 	w := webview.New(debug)
 	defer w.Destroy()
 	w.SetTitle("Minimal webview example")
-	w.SetSize(800, 600, webview.HintNone)
+	w.SetSize(width, height, webview.HintNone)
 	w.Navigate("http://localhost:3000")
 	w.Run()
 }
